36de946db8db577a6f45bb1e21b0abd1e0533eaa
cyh <1762375079@qq.com>
2025-07-19 01:11:13 +0800

    feat: 添加WebMvcEndpointHandlerMapping和相关方法
    添加了WebMvcEndpointHandlerMapping的bean定义，以及shouldRegisterLinksMapping方法。这些更改引入了新的依赖和导入，增强了Spring Boot Actuator的Web端点映射功能。

--- renren-fast-master/src/main/java/io/renren/config/SwaggerConfig.java
+++ renren-fast-master/src/main/java/io/renren/config/SwaggerConfig.java
@@ -8,9 +8,21 @@
 
 package io.renren.config;
 
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Sets;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.collections.MapUtils;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
+import org.springframework.boot.actuate.endpoint.web.*;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -20,7 +32,11 @@ import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import static com.google.common.collect.Lists.newArrayList;
@@ -56,6 +72,34 @@ public class SwaggerConfig implements WebMvcConfigurer {
         return newArrayList(
             new ApiKey("token", "token", "header")
         );
+    }
+
+    @Bean
+    public WebMvcEndpointHandlerMapping webMvcEndpointHandlerMapping(WebEndpointsSupplier wes
+            , ServletEndpointsSupplier ses, ControllerEndpointsSupplier ces, EndpointMediaTypes emt
+            , CorsEndpointProperties cep, WebEndpointProperties wep, Environment env) {
+        List<ExposableEndpoint<?>> allEndpoints = new ArrayList<>();
+        Collection<ExposableWebEndpoint> webEndpoints = wes.getEndpoints();
+        allEndpoints.addAll(webEndpoints);
+        allEndpoints.addAll(ses.getEndpoints());
+        allEndpoints.addAll(ces.getEndpoints());
+        String basePath = wep.getBasePath();
+        EndpointMapping endpointMapping = new EndpointMapping(basePath);
+        boolean shouldRegisterLinksMapping = shouldRegisterLinksMapping(wep, env, basePath);
+        return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, emt
+                , cep.toCorsConfiguration(), new EndpointLinksResolver(
+                allEndpoints, basePath), shouldRegisterLinksMapping, null);
+    }
+    /**
+     * shouldRegisterLinksMapping
+     *
+     * @param wep
+     * @param env
+     * @param basePath
+     * @return
+     */
+    private boolean shouldRegisterLinksMapping(WebEndpointProperties wep, Environment env, String basePath) {
+        return wep.getDiscovery().isEnabled() && (StringUtils.hasText(basePath) || ManagementPortType.get(env).equals(ManagementPortType.DIFFERENT));
     }
 
 }

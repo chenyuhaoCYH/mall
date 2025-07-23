fc2c2f1e2ff8f5520e7b940fc8cea34d627a483c
cyh <1762375079@qq.com>
2025-07-23 23:42:15 +0800

    1.renren-fast打通网关服务

--- mall-gateway/src/main/java/com/cyh/mall/gateway/MallGatewayApplication.java
+++ mall-gateway/src/main/java/com/cyh/mall/gateway/MallGatewayApplication.java
@@ -3,9 +3,10 @@ package com.cyh.mall.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.cyh.mall.gateway"})
 public class MallGatewayApplication {
 
     public static void main(String[] args) {
